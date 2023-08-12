16ecdccdf0ec684886a3f477dd6b9de57181b81d
fuad <fuamohaa@gmail.com>
2023-08-12 13:18:34 -0700

    second commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print new Data.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
